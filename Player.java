b1e77ed31f5db2f4f771db0f1fbb0c03b87ae12f
viktor <emilssonv@gmail.com>
2024-10-15 11:40:51 +0200

    småuppdateringar

--- Player.java
+++ Player.java
@@ -3,27 +3,34 @@ public class Player {
     private int level;
     private int experience;
     private int health;
+    private int strength;
+    private int speed;
 
     public Player(String name) {
         this.name = name;
         this.level = 1;
         this.experience = 0;
         this.health = 100;
+        this.strength = 10;
+        this.speed = 10;
     }
 
     public String getName() {
         return name;
     }
-
     public int getLevel() {
         return level;
     }
-
     public int getExperience() {
         return experience;
     }
-
     public int getHealth() {
         return health;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public int getSpeed() {
+        return speed;
     }
 }
