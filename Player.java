fff57ab297d5890ab98588b298fcab04f03697ac
viktor <emilssonv@gmail.com>
2024-10-15 11:04:54 +0200

    test

--- Player.java
+++ Player.java
@@ -1,2 +1,29 @@
 public class Player {
+    private String name;
+    private int level;
+    private int experience;
+    private int health;
+
+    public Player(String name) {
+        this.name = name;
+        this.level = 1;
+        this.experience = 0;
+        this.health = 100;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getHealth() {
+        return health;
+    }
 }
