7e15f744ea5b0bff8faeef0b9168b4e53b905002
albin <albin.bryn@gmail.com>
2024-10-15 10:57:20 +0200

    Lägger till Race klassen

---
+++ Race.java
@@ -0,0 +1,43 @@
+public abstract class Race {
+    private final String name;
+    private final int health;
+    private final int strength;
+    private final int speed;
+
+    //
+
+    public Race(String name, int health, int strength, int speed) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
+
+/*
+Race examples:
+
+Human
+Elf
+Giant
+Dwarf
+Gnome
+
+*/
+
