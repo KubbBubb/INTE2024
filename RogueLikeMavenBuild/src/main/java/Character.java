22625ec6d2dd54a141340e3123bbb2a91dae3472
viktor <emilssonv@gmail.com>
2024-10-17 10:48:29 +0200

    .

---
+++ RogueLikeMavenBuild/src/main/java/Character.java
@@ -0,0 +1,27 @@
+package RogueLikeMavenBuild.src.main.java;
+
+public class Character {
+    private String name;
+    private int level;
+    private int health;
+
+    public Character(String name) {
+        this.name = name;
+        this.level = 1;
+        this.health = 100;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+}
+
--- RogueLikeMavenBuild/src/main/java/Player.java
+++ RogueLikeMavenBuild/src/main/java/Player.java
@@ -1,38 +1,15 @@
 package RogueLikeMavenBuild.src.main.java;
 
-public class Player {
-    private String name;
-    private int level;
+public class Player extends Character {
     private int experience;
-    private int health;
-    private int strength;
-    private int speed;
 
     public Player(String name) {
-        this.name = name;
-        this.level = 1;
-        this.experience = 0;
-        this.health = 100;
-        this.strength = 10;
-        this.speed = 10;
+        super(name);
     }
 
-    public String getName() {
-        return name;
-    }
-    public int getLevel() {
-        return level;
-    }
     public int getExperience() {
-        return experience;
+        return this.experience;
     }
-    public int getHealth() {
-        return health;
-    }
-    public int getStrength() {
-        return strength;
-    }
-    public int getSpeed() {
-        return speed;
-    }
+
+
 }
