ecb1c75f7bd7b9a890fd645f6bc39b333046836d
viktor <emilssonv@gmail.com>
2024-10-28 17:38:21 +0100

    ny testklass

---
+++ RogueLikeMavenBuild/src/test/java/EntityClassTest.java
@@ -0,0 +1,26 @@
+package RogueLikeMavenBuild.src.test.java;
+
+import RogueLikeMavenBuild.src.main.java.Character;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EntityTest {
+
+    @Test
+    public void testEntityHasName() {
+        Entity entity = new entity(“Testname”);
+        assertEquals(“Testname”, entity.getName(), "Character should have a name");
+    }
+
+    @Test
+    public void testEntityInitialHealth() {
+        Entity entity = new Entity(“Testname”);
+        assertEquals(100, entity.getHealth(), "Character should have initial health of 100");
+    }
+
+    @Test
+    public void testEntityInitialLevel() {
+        Entity entity = new Entity(“Hero");
+                assertEquals(1, entity.getLevel(), "Character should have initial level of 1");
+    }
+}
