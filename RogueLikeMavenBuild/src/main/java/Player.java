39cf43d3de178c6177f0af6ddd232495bbef4461
viktor <emilssonv@gmail.com>
2024-10-28 14:39:24 +0100

    skapat Character-klassen

--- RogueLikeMavenBuild/src/main/java/Player.java
+++ RogueLikeMavenBuild/src/main/java/Player.java
@@ -11,5 +11,23 @@ public class Player extends Character {
         return this.experience;
     }
 
+    public void addExperience(void amount) {
+        this.experience += amount;
+
+        while (this.experience >= 100) {
+            this.experience -= 100;
+            levelUp();
+        }
+    }
+
+    private void levelUp() {
+        this.setLevel(this.getLevel() + 1);
+    }
+
+    private void setLevel(int newLevel) {
+        super.level = newLevel;
+    }
+}
+
 
 }
--- RogueLikeMavenBuild/src/test/java/PlayerClassTest.java
+++ RogueLikeMavenBuild/src/test/java/PlayerClassTest.java
@@ -1,4 +1,56 @@
 package RogueLikeMavenBuild.src.test.java;
 
+import RogueLikeMavenBuild.src.main.java.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlayerClassTest {
+    private Player player;
+
+    @BeforeEach
+    public void setUp() {
+        player = new Player("TestPlayer");
+    }
+
+    @Test
+    public void testInitialExperience() {
+        assertEquals(0, player.getExperience(), “Base experience should be 0");
+    }
+
+    @Test
+    public void testAddExperienceUnderLevelUpThreshold() {
+        player.addExperience(50);
+        assertEquals(50, player.getExperience(), "Experience should be 50 after adding 50");
+        assertEquals(1, player.getLevel(), "Level should remain 1 when experience is below 100");
+    }
+
+    @Test
+    public void testAddExperienceLevelUp() {
+        player.addExperience(120);
+        assertEquals(20, player.getExperience(), "Experience should be 20 after leveling up with 120 experience");
+        assertEquals(2, player.getLevel(), "Level should be 2 after gaining 100 experience");
+    }
+
+    @Test
+    public void testAddExperienceMultipleLevelUps() {
+        player.addExperience(250);
+        assertEquals(50, player.getExperience(), "Experience should be 50 after leveling up with 250 experience");
+        assertEquals(3, player.getLevel(), "Level should be 3 after gaining 250 experience");
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("TestPlayer", player.getName(), "Name should be 'TestPlayer'");
+    }
+
+    @Test
+    public void testInitialHealth() {
+        assertEquals(100, player.getHealth(), "Initial health should be 100");
+    }
+
+    @Test
+    public void testInitialLevel() {
+        assertEquals(1, player.getLevel(), "Initial level should be 1");
+    }
 }
