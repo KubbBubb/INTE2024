909db638aace88d01f58c34853b6f45ef9371995
Sawera18 <saweraidrees34@gmail.com>
2024-10-30 01:52:01 +0100

    Update Terrain.java

--- RogueLikeMavenBuild/src/main/java/rougelike/Terrain.java
+++ RogueLikeMavenBuild/src/main/java/rougelike/Terrain.java
@@ -5,22 +5,23 @@ public class Terrain {
     private boolean isExplored;
     private String terrainType;
     private int height;
-    private String accessType;
+    //private String accessType;
+
+    //public Terrain(String terrainType, int height) {
+     //   this(terrainType, height,"walk");
+   // }
 
     public Terrain(String terrainType, int height) {
-        this(terrainType, height, "walk");
-    }
-
-    public Terrain(String terrainType, int height, String accessType) {
         this.terrainType = terrainType;
         this.height = height;
         this.isVisible = false;
         this.isExplored = false;
-        this.accessType = accessType;
+        //this.accessType = accessType;
     }
-    public boolean isAccessibleBy(String characterType) {
-        return characterType.equals(this.accessType) || "any".equals(this.accessType);
-    }
+   // public boolean isAccessibleBy(Entity entity) {
+      //  return entity.getCharacterType().equals(this.accessType) || "any".equals(this.accessType);
+   // }
+
 
     public boolean isVisible() {
         return isVisible;
