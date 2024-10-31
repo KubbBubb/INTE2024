c228957aa8f3f468ec2af710c2d0d68e61d5b49d
KubbBubb <133816844+KubbBubb@users.noreply.github.com>
2024-10-31 12:56:20 +0100

    Börjat utöka CombatTest

--- RogueLikeMavenBuild/src/main/java/rougelike/combat/Action.java
+++ RogueLikeMavenBuild/src/main/java/rougelike/combat/Action.java
@@ -7,7 +7,7 @@ public class Action {
     public static void hit(GameCharacter actionChar, GameCharacter reactiveChar){
 
         int baseDamage = 10;
-        double totalDamage = baseDamage * actionChar.getStrength();
+        double totalDamage = baseDamage * actionChar.getRace().getStrengthModifier();
 
         double effect = reactiveChar.getHealth() - totalDamage;
         reactiveChar.setHealth(effect);
--- RogueLikeMavenBuild/src/test/java/CombatTest.java
+++ RogueLikeMavenBuild/src/test/java/CombatTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import rougelike.GameCharacter;
@@ -7,23 +9,40 @@ import rougelike.races.Elf;
 import rougelike.races.Giant;
 import rougelike.races.Human;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static rougelike.combat.Action.hit;
+import static rougelike.combat.Initiative.initativeOrder;
+
 
 public class CombatTest {
+
     @Test
-    @DisplayName("Testar att ge slag mot en rougelike.Entity")
+    @DisplayName("Testar att utdela ett slag. PC mot NPC ")
     void testUsingActionHit(){
 
-
-        var dwarf = new Dwarf();
-        var elf = new Elf();
-        var giant = new Giant();
         var human = new Human();
 
+        var pc = new Player("test pc", human);
+        var npc = new GameCharacter("test npc", human);
 
-        var pc = new Player("test pc", dwarf);
-        var npc = new GameCharacter("test npc", elf);
+        double remainingHp =  npc.getHealth() - (npc.getRace().getStrengthModifier() * 10);
+        hit(pc, npc);
 
-        //assertEquals();
+        assertEquals(90, remainingHp);
+
+    }
+
+    @Test
+    @DisplayName("Testar att initativet i strid fungerar")
+    void testCombatSpeedInitiative(){
+
+        var human = new Human();
+        var elf = new Elf();
+
+        var pc = new Player("test pc", elf);
+        var npc = new GameCharacter("test npc", human);
+
+        assertEquals(pc, initativeOrder(pc, npc));
 
     }
 
