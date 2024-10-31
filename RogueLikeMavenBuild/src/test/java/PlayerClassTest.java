4f7d26e630ad8b6b9092b51c5724903c12e76384
viktor <emilssonv@gmail.com>
2024-10-31 18:03:31 +0100

    minifix

--- RogueLikeMavenBuild/src/test/java/PlayerClassTest.java
+++ RogueLikeMavenBuild/src/test/java/PlayerClassTest.java
@@ -47,7 +47,7 @@ public class PlayerClassTest {
 
     @Test
     public void testInitialHealth() {
-        assertEquals(150, player.getHealth(), "Initial health should be 100");
+        assertEquals(150, player.getHealth(), "Initial health should be 150 for a Dwarf");
     }
 
     @Test
